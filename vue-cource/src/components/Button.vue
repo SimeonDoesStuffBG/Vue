7af9a928295022c65b763974624085aac00a6fd9
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-09-11 23:59:25 +0300

    Added header

---
+++ vue-cource/src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="addTask()" :style="{background:color}" class="btn">{{text}}</button>
+</template>
+
+<script lang="ts">
+    export default{
+        name:"Button",
+        props:{
+            text:{
+                type:String,
+                default:"Add Task"
+            },
+            color:String
+        },
+        methods:{
+            addTask(){
+                console.log("Hi");
+            }
+        },
+    }
+</script>
---
+++ vue-cource/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button color="green"/>
+    </header>
+</template>
+
+<script lang="ts">
+    import Button from './Button.vue';
+
+    export default{
+        name:'Header',
+        props:{
+            title:String,
+        },
+        components:{
+            Button,
+        }
+    }
+</script>
+
+<style scoped>
+    header{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
